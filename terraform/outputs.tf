96ebc1478b0217aa615b057c30cca3d38401e977
ranygo12-pixel <ranygo12@gmail.com>
2026-02-09 15:26:09 +0900

    Rename output from 'agent_private_ips' to 'agent_private_ip'

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,7 @@ output "server_public_ip" {
   value       = aws_instance.k3s_server.public_ip
 }
 
-output "agent_private_ips" {
+output "agent_private_ip" {
   description = "내부에서 확인 가능한 K3s Agent IP 목록"
   value       = aws_instance.k3s_agent[*].private_ip
 }
