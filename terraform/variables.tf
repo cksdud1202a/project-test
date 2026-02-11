19348f0c527f99719afae609b2ccfbc1283652e4
kcy <cksdud1202a@naver.com>
2026-02-10 19:48:54 -0500

    test1

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "agent_count" { default = 2 }
 # 프리티어 유지를 위해 micro로 설정 (Swap으로 부족한 RAM 보완)
 variable "instance_type" { default = "t3.micro" }
 
-variable "key_name" { default = "my-terraform-key" } # 실제 보유한 키페어 이름
+variable "key_name" { default = "mini-project-user" } # 실제 보유한 키페어 이름
 
 # K3s 노드 간 인증을 위한 비밀 토큰
 variable "k3s_token" { default = "k3s-token-2026-secret" }
