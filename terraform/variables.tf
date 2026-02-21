09e678e37363f0c723bc3ad7637d3ac43dcabf9d
ranygo12-pixel <ranygo12@gmail.com>
2026-02-22 01:50:15 +0900

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@ variable "project_name" { default = "k3s-project" }
 variable "agent_count" { default = 2 }
 
 # 프리티어 유지를 위해 micro로 설정 (Swap으로 부족한 RAM 보완)
-variable "instance_type" { default = "t3.micro" }
+variable "instance_type" { default = "t4g.small" }
 
 variable "key_name" { default = "my-terraform-key" } # 실제 보유한 키페어 이름
 
